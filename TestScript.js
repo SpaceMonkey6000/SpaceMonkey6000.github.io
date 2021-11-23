b39fd073de6a7d1ce5ba3b2c0ba21aa9aadfa265
Deven <devenjain2020@gmail.com>
2021-11-23 10:28:09 +0530

    Made minor text modification to TestScript.js

--- TestScript.js
+++ TestScript.js
@@ -1,4 +1,4 @@
-let ans = prompt("Hi Aaron! Answer this question:\nHow old are you?>");
+let ans = prompt("Hi Aaron! Answer this question:\nHow old are you?");
 if (ans<18){
     document.getElementById("isDrink").innerHTML='You are not even an adult and you are thinking of drinking!? I will tell your mom!';
 } else if(ans<21){
